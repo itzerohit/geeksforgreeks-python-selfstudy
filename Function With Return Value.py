650179605e23af6899610385bca0e697aebc8590
Rohit Kumar <152129671+itzerohit@users.noreply.github.com>
2025-07-29 03:39:32 +0530

    Create Function With Return Value.py

---
+++ Function With Return Value.py
@@ -0,0 +1,15 @@
+Function With Return Value
+Difficulty: BasicAccuracy: 62.86%Submissions: 38K+Points: 1Average Time: 10m
+Here one integer n is given. You need to write the complete function returnValueFunction that takes n as a parameter and uses the return keyword to return double the value of n.
+
+Examples:
+
+Input: n = 2
+Output: 4
+Explanation: 2 * 2 = 4
+
+Ans -- 
+#code here
+def returnValueFunction(x):
+    return 2*x
+    
