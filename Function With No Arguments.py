0a5afe68ef35d75c5dcc0662aaf944584fa66779
Rohit Kumar <152129671+itzerohit@users.noreply.github.com>
2025-07-29 03:42:53 +0530

    Create Function With No Arguments.py

---
+++ Function With No Arguments.py
@@ -0,0 +1,20 @@
+Function With No Arguments
+Difficulty: BasicAccuracy: 74.22%Submissions: 20K+Points: 1Average Time: 10m
+This problem has no input. You need to write the function helloFunction that prints Hello.
+
+Examples: 
+
+Input: None
+Output: Hello
+Explanation: There is no input. The function prints Hello everytime.
+
+Ans -- 
+
+#User function Template for python3
+
+#Write the helloFunction below. It should have one statement
+#print("Hello")
+#code here
+
+def helloFunction():
+    print("Hello")
